519d8a7892a9eb96f1655f5cab4752aed5895811
Tapani Voutilainen <ltvoutilainen@outlook.com>
2023-09-21 10:41:11 +0300

    connection returns tables

--- index.js
+++ index.js
@@ -2,9 +2,26 @@ require("dotenv").config();
 const express = require("express");
 const PORT = process.env.PORT;
 const app = express();
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.DBUSER,
+  port: process.env.DBPORT,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  insecureAuth: true,
+});
 
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+  connection.connect();
+
+  connection.query("show tables;", function (error, results, fields) {
+    if (error) throw error;
+    res.json(results);
+  });
+
+  connection.end();
 });
 
 app.listen(PORT, () => {
