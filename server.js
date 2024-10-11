3ca75a1238c04686bc7f0879742b970fe4723ea4
Tapani Voutilainen <ltvoutilainen@outlook.com>
2024-10-11 14:38:35 +0300

    added cors

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql2");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(cors());
 
 const pool = mysql.createPool({
   host: process.env.HOST,
