87f2ec170fd388702b3fc923fe59b34bea10aca6
Tapani Voutilainen <ltvoutilainen@outlook.com>
2025-01-26 22:07:39 +0200

    modified tests

--- test/server.test.js
+++ test/server.test.js
@@ -1,30 +1,30 @@
-require("dotenv").config();
-const request = require("supertest");
-const app = require("../server");
-const randomtable = require(`./${process.env.DB_RANDOM_TABLE_NAME_JSON_FILE}`);
-const alltables = require("./alltables");
+require('dotenv').config()
+const request = require('supertest')
+const app = require('../server')
+const randomtable = require(`./${process.env.DB_RANDOM_TABLE_NAME_JSON_FILE}`)
+const alltables = require('./alltables')
 
-describe("api", () => {
-  it("should return tables", (done) => {
+describe('api', () => {
+  it('should return tables', (done) => {
     request(app)
-      .get("/api")
-      .expect("Content-Type", /json/)
+      .get('/_api')
+      .expect('Content-Type', /json/)
       .expect(200, alltables)
       .end((err, _res) => {
-        if (err) throw err;
-        done();
-      });
-  });
+        if (err) throw err
+        done()
+      })
+  })
 
   it(`should return the ${process.env.DB_RANDOM_TABLE_NAME_JSON_FILE}`, (done) => {
     request(app)
-      .post("/api")
+      .post('/_api')
       .send({ table: `${process.env.DB_RANDOM_TABLE_NAME}` })
-      .expect("Content-Type", /json/)
+      .expect('Content-Type', /json/)
       .expect(200, randomtable)
       .end((err, _res) => {
-        if (err) throw err;
-        done();
-      });
-  });
-});
+        if (err) throw err
+        done()
+      })
+  })
+})
